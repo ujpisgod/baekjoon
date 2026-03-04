04aadcb1dac7b67828059de54144144d7c049331
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-04 12:04:42 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ Python/백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,14 +1,2 @@
-x,y,a,b=map(int,input().split())
-m=[x,x,y,y]
-mm=[a,0,b,0]
-_try=0
-while True:
-    m[0]+=1
-    m[1]-=1
-    m[2]+=1
-    m[3]-=1
-    _try+=1
-    for i in range(4):
-        if m[i]==mm[i]:
-            print(_try)
-            exit()
+x, y, a, b = map(int, input().split())
+print(min(x, y, a - x, b - y))
