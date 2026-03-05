29d1766de6bc05ea4dec163c62f94dbb9b371706
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-05 15:24:03 +0900

    [Silver V] Title: 영화감독 숌, Time: 636 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+n=int(input())
+t=1665
+r=1
+if n==1:
+    print(666)
+else:
+    while True:
+        if '666' in str(t): 
+            r+=1
+        if r==n:
+            print(t)
+            break
+        t+=1
