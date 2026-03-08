c806895297c84d5fa495150e80f57bb7ef9a36ce
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-08 21:30:48 +0900

    [Bronze II] Title: SciComLove (2023), Time: 3232 ms, Memory: 35712 KB, Score: 30 point -BaekjoonHub

--- Python/백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.py
+++ Python/백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.py
@@ -4,10 +4,11 @@ a1=[]
 for i in range(m):
     a1.extend([1,0,0,1,0,0,1,0,0,0])
 a2=a1[0:n]
+s=sum(a2)
 for i in range(a):
     c=int(input())-1
     if a2[c]==0:
-        a2[c]=1
+        s+=1
     else:
-        a2[c]=0
-    print(sum(a2))
+        s-=1
+    print(s)
