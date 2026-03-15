f8b5cca1a8015b6b83e9db0f1fe67f65960667fd
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-15 19:07:13 +0900

    [Bronze I] Title: 색종이, Time: 408 ms, Memory: 207768 KB, Score: 53 point -BaekjoonHub

--- PyPy3/백준/Bronze/10163. 색종이/색종이.py
+++ PyPy3/백준/Bronze/10163. 색종이/색종이.py
@@ -1,8 +1,11 @@
 n=int(input())
 p=set()
 pp=[0]*n
+papers = []
+for _ in range(n):
+    papers.append(tuple(map(int, input().split())))
 for i in range(n-1,-1,-1):
-    a,b,s,t=map(int,input().split())
+    a,b,s,t=papers[i]
     for j in range(s):
         for k in range(t):
             if (a+j,b+k) not in p:
