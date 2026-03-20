dac709a26221c816165ae50cfbaa8912db7a5fcb
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-21 01:17:05 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 33192 KB -BaekjoonHub

--- Python/백준/Silver/10773. 제로/제로.py
+++ Python/백준/Silver/10773. 제로/제로.py
@@ -1,9 +1,15 @@
-n=int(input())
-a=[]
+import sys
+# input을 sys의 빠른 버전으로 교체!
+input = sys.stdin.readline
+
+n = int(input())
+a = []
 for i in range(n):
-    b=int(input())
-    if b==0:
+    # 이제부터는 10만 개를 읽어도 눈 깜짝할 새에 끝남
+    b = int(input())
+    if b == 0:
         a.pop()
     else:
         a.append(b)
+
 print(sum(a))
