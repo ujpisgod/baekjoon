67a72b5dbd6d86cfb3d6f3d94a57c57a68accd02
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-25 03:31:06 +0900

    [Silver I] Title: 경로 찾기, Time: 236 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,10 @@
+n=int(input())
+graph=[list(map(int,input().split())) for i in range(n)]
+    
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] == 1 and graph[k][j] == 1:
+                graph[i][j] = 1
+for i in graph:
+    print(*i)
