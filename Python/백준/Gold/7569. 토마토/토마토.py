bd758d2c0090c01f43b4e7bc905855b9029c7393
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-26 01:39:54 +0900

    [Gold V] Title: 토마토, Time: 4148 ms, Memory: 58496 KB -BaekjoonHub

--- Python/백준/Gold/7569. 토마토/토마토.py
+++ Python/백준/Gold/7569. 토마토/토마토.py
@@ -1,7 +1,9 @@
+import sys
 from collections import deque as dq
+input=sys.stdin.readline
+
 x,y,z=map(int,input().split())
 al=[[list(map(int,input().split())) for i in range(y)] for i in range(z)]
-visit=[[[False]*x for i in range(y)] for i in range(z)]
 day=[[[-1]*x for i in range(y)] for i in range(z)]
 q=dq()
 for i in range(z):
@@ -10,19 +12,15 @@ for i in range(z):
             if al[i][j][k]==1:
                 q.append((i,j,k))
                 day[i][j][k]=0
-            if al[i][j][k]==-1:
-                visit[i][j][k]=True
 dx=[1,-1,0,0,0,0]
 dy=[0,0,1,-1,0,0]
 dz=[0,0,0,0,1,-1]
 while q:
     num=1
     m=q.popleft()
-    visit[m[0]][m[1]][m[2]]=True
     for i in range(6):
         nz,ny,nx=m[0]+dz[i],m[1]+dy[i],m[2]+dx[i]
-        if 0<=nz<z and 0<=ny<y and 0<=nx<x and visit[nz][ny][nx]==False and al[nz][ny][nx]==0:
-            visit[nz][ny][nx]=True
+        if 0<=nz<z and 0<=ny<y and 0<=nx<x and al[nz][ny][nx]==0:
             al[nz][ny][nx]=1
             q.append((nz,ny,nx))
             day[nz][ny][nx]=day[m[0]][m[1]][m[2]]+1
