c7295df3f7c07934699b1f1f2f65d2b25fcaf42e
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-03-26 01:34:26 +0900

    [Gold V] Title: 토마토, Time: 4984 ms, Memory: 67488 KB -BaekjoonHub

---
+++ Python/백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,41 @@
+from collections import deque as dq
+x,y,z=map(int,input().split())
+al=[[list(map(int,input().split())) for i in range(y)] for i in range(z)]
+visit=[[[False]*x for i in range(y)] for i in range(z)]
+day=[[[-1]*x for i in range(y)] for i in range(z)]
+q=dq()
+for i in range(z):
+    for j in range(y):
+        for k in range(x):
+            if al[i][j][k]==1:
+                q.append((i,j,k))
+                day[i][j][k]=0
+            if al[i][j][k]==-1:
+                visit[i][j][k]=True
+dx=[1,-1,0,0,0,0]
+dy=[0,0,1,-1,0,0]
+dz=[0,0,0,0,1,-1]
+while q:
+    num=1
+    m=q.popleft()
+    visit[m[0]][m[1]][m[2]]=True
+    for i in range(6):
+        nz,ny,nx=m[0]+dz[i],m[1]+dy[i],m[2]+dx[i]
+        if 0<=nz<z and 0<=ny<y and 0<=nx<x and visit[nz][ny][nx]==False and al[nz][ny][nx]==0:
+            visit[nz][ny][nx]=True
+            al[nz][ny][nx]=1
+            q.append((nz,ny,nx))
+            day[nz][ny][nx]=day[m[0]][m[1]][m[2]]+1
+m1=0
+lll=0
+for i in range(z):
+    for j in range(y):
+        for k in range(x):
+            m1=max(m1,day[i][j][k])
+            if al[i][j][k]==0:
+                lll=-1
+if lll!=-1:
+    print(m1)
+else:
+    print(-1)
+    
