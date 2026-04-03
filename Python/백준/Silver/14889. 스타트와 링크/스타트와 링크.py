5f5e5e71ef6e545bb0346c18d7079532fe75f204
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-04-03 19:00:08 +0900

    [Silver I] Title: 스타트와 링크, Time: 2852 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,24 @@
+n=int(input())
+l=[list(map(int,input().split())) for i in range(n)]
+s=[]
+t=[]
+g=1e6
+ll=list(range(1,n+1))
+def solve(player):
+    global g
+    if len(s)==n//2:
+        t=list(set(ll) - set(s))
+        sc1,sc2 = 0,0
+        for i in s:
+            for j in s:
+                sc1+=l[i-1][j-1]
+        for i in t:
+            for j in t:
+                sc2+=l[i-1][j-1]
+        g=min(g,abs(sc1-sc2))
+    for i in range(player,n+1):
+        s.append(i)
+        solve(i+1)
+        s.pop()
+solve(1)
+print(g)
