f3914375c5e5936f3e9613ba41938d8cf24850a5
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-04-06 01:54:44 +0900

    [Gold IV] Title: 트리의 지름, Time: 64 ms, Memory: 37992 KB -BaekjoonHub

---
+++ Python/백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**6) 
+input = sys.stdin.readline
+
+n = int(input())
+l = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    v1, v2, w = map(int, input().split())
+    l[v1].append((v2, w))
+    l[v2].append((v1, w))
+def solve(now, dist):
+    for next_node, weight in l[now]:
+        if visit[next_node] == -1:
+            visit[next_node] = dist + weight
+            solve(next_node, dist + weight)
+visit = [-1] * (n + 1)
+visit[1] = 0
+solve(1, 0)
+start_node = 0
+max_dist = -1
+for i in range(1, n + 1):
+    if visit[i] > max_dist:
+        max_dist = visit[i]
+        start_node = i
+visit = [-1] * (n + 1)
+visit[start_node] =0
+solve(start_node, 0)
+print(max(visit))
