d9ee599a6d43d8d3b4ce360d274be5f683297cbb
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-04-07 21:55:50 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 112 ms, Memory: 38124 KB -BaekjoonHub

--- Python/백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ Python/백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,30 +1,33 @@
-from collections import deque as dq
-a,b=map(int,input().split())
-if a==b:
-    print(0)
-    print(1)
-    exit()
-visit=[-1]*(10**5+1)
-INF=int(1e9)
-dp=[0]*(10**5+1)
-dp[a]=1
-q=dq([])
-q.append(a)
-visit[a]=0
-limit=INF
-while q:
-    t=q.popleft()
-    m=[t-1,t+1,2*t]
-    for i in m:
-        if 0<=i<=100000:
-            if visit[i]==-1 and visit[t]+1<limit:
-                visit[i]=visit[t]+1
-                dp[i]=dp[t]
-                if i!=b:
-                    q.append(i)
-                else:
-                    limit=visit[i]
-            elif visit[i]==visit[t]+1:
-                dp[i]+=dp[t]
-print(visit[b])
-print(dp[b])
+import sys
+from collections import deque
+
+def solve():
+    input=sys.stdin.readline
+    a,b=map(int, input().split())
+    if a==b:
+        print(0)
+        print(1)
+        return
+    visit=[-1]*100001
+    dp=[0]*100001
+    visit[a]=0
+    dp[a]=1
+    q=deque([a])
+    limit=1e9  # INF 값
+    while q:
+        t = q.popleft()
+        for i in (t-1, t+1, t*2):
+            if 0 <= i <= 100000:
+                if visit[i] == -1 and visit[t] + 1 < limit:
+                    visit[i] = visit[t] + 1
+                    dp[i] = dp[t]
+                    if i != b:
+                        q.append(i)
+                    else:
+                        limit = visit[i]
+                elif visit[i] == visit[t] + 1:
+                    dp[i] += dp[t]
+    print(visit[b])
+    print(dp[b])
+if __name__ == '__main__':
+    solve()
