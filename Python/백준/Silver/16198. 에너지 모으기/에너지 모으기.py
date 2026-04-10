5ea6ded4463f5a68aca07a8b194a883548aed227
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-04-10 13:32:48 +0900

    [Silver I] Title: 에너지 모으기, Time: 208 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ Python/백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -1,26 +1,31 @@
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-l = list(map(int, input().split()))
-max_energy = 0
-
-def solve(current_list, current_sum):
-    global max_energy
-    
-    # 1. 종료 조건: 구슬이 2개만 남으면 끝!
-    if len(current_list) == 2:
-        max_energy = max(max_energy, current_sum)
+n=int(input())
+l=list(map(int,input().split()))
+ans=[]
+k=0
+s=0
+def solve():
+    global k, s
+    if len(ans)==n-2:
+        k=max(k,s)
         return
-    
-    # 2. 첫 번째와 마지막 구슬은 못 고르니까 1부터 len-2까지
-    for i in range(1, len(current_list) - 1):
-        # 에너지 계산: 양옆의 곱 (문제에서 곱하기라고 했어!)
-        energy = current_list[i-1] * current_list[i+1]
-        
-        # 3. i번째 구슬을 뺀 '새 리스트'를 만들어서 다음 재귀로 토스!
-        # current_list 자체는 변하지 않아서 for문이 꼬이지 않아.
-        solve(current_list[:i] + current_list[i+1:], current_sum + energy)
-
-solve(l, 0)
-print(max_energy)
+    for i,t in enumerate(l):
+        if i in ans:
+            continue
+        elif i==0:
+            continue
+        elif i==(n-1):
+            continue
+        else:
+            ans.append(i)
+            left=i-1
+            right=i+1
+            while left in ans:
+                left-=1
+            while right in ans:
+                right+=1
+            s+=l[left]*l[right]
+            solve()
+            ans.pop()
+            s-=l[left]*l[right]
+solve()
+print(k)
