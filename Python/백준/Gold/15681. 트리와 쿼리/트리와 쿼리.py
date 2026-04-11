b0698bd3d91fa7b1670c938a1d820cab79fe59bb
ujpisgod <87768098+ujpisgod@users.noreply.github.com>
2026-04-11 23:39:17 +0900

    [Gold V] Title: 트리와 쿼리, Time: 384 ms, Memory: 70136 KB -BaekjoonHub

--- Python/백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ Python/백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -8,17 +8,12 @@ for i in range(v-1):
     l[a].append(b)
     l[b].append(a)
 dp=[0]*(v+1)
-visit=[False]*(v+1)
-def solve(x):
-    visit[x]=True
-    if len(l[x])==1 and x!=r:
-        dp[x]=1
-        return dp[x]
-    dp[x] = 1
+def solve(x,p):
+    dp[x]=1
     for i in l[x]:
-        if visit[i]==False:
-            dp[x]+=solve(i)
+        if i!=p:
+            dp[x]+=solve(i,x)
     return dp[x]
-solve(r)
+solve(r,0)
 for i in range(n):
     print(dp[int(input())])
